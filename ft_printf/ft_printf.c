eb668d736257d51ab1168223ebacb907d76c22c5
Naomi Roth <nroth@c2r6s5.42prague.com>
2023-01-26 13:39:02 +0100

    added logic to ft_printf,text file with flag, type descriptors

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,42 @@
+#include "ft_printf.h"
+
+/*
+	get cut format str, send to printing funcd
+	updates char count to add however many chars were printed
+	returns format str len, -1 on err
+	FREE format str
+*/
+int	print_format(char *format_str, int *char_count);
+/*
+	get format str from s[i] up until its end, allocate it
+	can either be one letter or a bunch of numbers/points/flags
+	return it
+*/
+char *grab_f_str();
+int	ft_printf(const char *str, ...)
+{
+	int	char_count;
+	int	i;
+	va_list va_ptr;
+
+	i = 0;
+	char_count = 0;
+	va_start(va_ptr, str);
+
+	while (str[i] && char_count >= 0)
+	{
+		if (str[i] == '%')
+		{
+			//get format str len
+			// printthe stuff, check output len
+			// i+= format str len
+			// char_count += output len
+			i += print_format(grab_f_str(str[i + 1]), &char_count);			
+		}
+		else
+			ft_putchar_fd(str[i], 1);
+		i++;
+	}
+	va_end(va_ptr);
+	return (char_count);
+}
---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nroth <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 13:16:12 by nroth             #+#    #+#             */
+/*   Updated: 2023/01/26 13:16:13 by nroth            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "./Libft/libft.h"
+
+//MAINFILE
+int	ft_printf(const char *str, ...);
+
+#endif
