db2b452e6a22008931fb9e4f6e491635f03e84f0
noona <naomindel@gmail.com>
2023-04-03 08:12:22 +0200

    added comments in exam gnl

--- exam_gnl.c
+++ exam_gnl.c
@@ -5,11 +5,12 @@
 #include <unistd.h>
 
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE 9
+# define BUFFER_SIZE 9
 #endif
 
 void ft_strcpy(char *src, char *dest)
 {
+	//MAKE SURE STRCPY NULL TERMINATES DEST
 	if (!src || !*src)
 		dest[0] = 0;
 	while (*src)
@@ -36,7 +37,7 @@ char *ft_strchr(char *src, char c)
 
 char	*get_next_line(int fd)
 {
-	if (BUFFER_SIZE <= 0 || fd < 0)
+	if (fd < 0)
 		return(NULL);
 
 	static char	*leftover;
@@ -56,13 +57,13 @@ char	*get_next_line(int fd)
 
 	while((rd = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
-		buffer[rd] = 0;
-		ft_strcpy(buffer, line_end);
-		line_end = ft_strchr(line, 0);
+		buffer[rd] = 0;		//IMPORTANT TO NULL TERMINATE
+		ft_strcpy(buffer, line_end);	//append buffer to line
+		line_end = ft_strchr(line, 0);	//update line end to be the line's null terminator
 		if (ft_strchr(line, '\n'))
 			break;
 	}
-
+	//WHEN FREEING LEFTOVERS, MAKE IT NULL SO DOESNT GET CAUGHT IN IF STATEMENT
 	if (rd == 0 && !line[0]) 				// nothing was read
 		return(free(buffer), free(leftover), leftover = NULL, free(line), NULL);
 	if (rd == -1)							// reading err
@@ -71,7 +72,7 @@ char	*get_next_line(int fd)
 		return(free(buffer), free(leftover), leftover = NULL, line);
 
 	//otherwise, copy leftover to leftover static var, null terminate LINE and return it
-	line_end = ft_strchr(line, '\n') + 1;
+	line_end = ft_strchr(line, '\n') + 1; //one after the nl char
 	ft_strcpy(line_end, leftover);
 	*line_end = '\0';
 	return(free(buffer), line);
