3d873183a3d3d13bef245011c155f2f046d6d786
Naomi Roth <nroth@c2r4s5.42prague.com>
2023-05-29 15:39:32 +0200

    microshell

---
+++ microshell/microshell.h
@@ -0,0 +1,11 @@
+#ifndef MICROSHELL_H
+#define MICROSHELL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+#endif
